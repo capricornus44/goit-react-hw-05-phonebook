f831744ed41d2ec00344a17db98e15b882c2d111
capricornus44 <antonzavalniuk@gmail.com>
2021-04-10 10:44:37 +0200

    import basic styles and modern-normalize.css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from './Components/App';
 
+import 'modern-normalize/modern-normalize.css';
+import './styles/base.scss';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
