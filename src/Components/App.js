7932c6039e16bb71da98857890c1b21703f1014e
capricornus44 <antonzavalniuk@gmail.com>
2021-04-11 00:44:22 +0200

    Add class-component 'App' with styles

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Form from './form/Form';
+// import Filter from './filter/Filter';
+// import Contacts from './contacts/Contacts';
 
-const App = () => {
-  return (
-    <h1>Template</h1>
-  );
+import { AppContainer } from './AppStyled';
+
+import {
+  getContacts,
+  addContact,
+  // deleteContact,
+} from '../services/dbRequest';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    error: '',
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await getContacts();
+      if (response.data) {
+        const contacts = Object.keys(response.data).map(key => ({
+          id: key,
+          ...response.data[key],
+        }));
+        this.setState({ contacts });
+      }
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      // this.setState({ isLoading: false });
+    }
+  }
+
+  handleAddContact = async contact => {
+    try {
+      const response = await addContact(contact);
+      this.setState(prevState => ({
+        contacts: [
+          ...prevState.contacts,
+          { ...contact, id: response.data.name },
+        ],
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      // this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        <section className="section">
+          <h1>Phonebook</h1>
+          <Form addContact={this.handleAddContact} />
+        </section>
+
+        {/* <section>
+          <Filter />
+        </section> */}
+
+        {/* <section title="Contacts">
+          <ContactList />
+        </section> */}
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/AppStyled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 1170px;
+  height: 100vh;
+  padding: 0 15px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 80px;
+
+  .section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 350px;
+    height: auto;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #ffffffea;
+    box-shadow: 0 4px 6px 4px rgba(0, 0, 0, 0.2);
+
+    &:not(:last-child) {
+      margin-right: 32px;
+    }
+  }
+`;
